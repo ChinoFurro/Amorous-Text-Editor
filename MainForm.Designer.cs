20c1795743c13ab053bf4b1d6bbe27afc8c93426
BunnyKyra <sasamedeilan@gmail.com>
2025-07-04 07:00:36 -0300

    New Vercion

---
+++ MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TraducAmorous
+{
+    partial class MainForm
+    {
+
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.PathColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OriginalTextColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TranslatedTextColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PathColumn,
+            this.OriginalTextColumn,
+            this.TranslatedTextColumn});
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(776, 397);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // PathColumn
+            // 
+            this.PathColumn.DataPropertyName = "Path";
+            this.PathColumn.HeaderText = "Ubicación en JSON";
+            this.PathColumn.Name = "PathColumn";
+            this.PathColumn.ReadOnly = true;
+            this.PathColumn.Width = 200;
+            // 
+            // OriginalTextColumn
+            // 
+            this.OriginalTextColumn.DataPropertyName = "OriginalText";
+            this.OriginalTextColumn.HeaderText = "Texto Original";
+            this.OriginalTextColumn.Name = "OriginalTextColumn";
+            this.OriginalTextColumn.ReadOnly = true;
+            this.OriginalTextColumn.Width = 300;
+            // 
+            // TranslatedTextColumn
+            // 
+            this.TranslatedTextColumn.DataPropertyName = "TranslatedText";
+            this.TranslatedTextColumn.HeaderText = "Texto Traducido";
+            this.TranslatedTextColumn.Name = "TranslatedTextColumn";
+            this.TranslatedTextColumn.Width = 300;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOpen.Location = new System.Drawing.Point(12, 415);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 30);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Abrir JSON";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSave.Location = new System.Drawing.Point(118, 415);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 30);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSaveAs.Location = new System.Drawing.Point(224, 415);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(100, 30);
+            this.btnSaveAs.TabIndex = 3;
+            this.btnSaveAs.Text = "Guardar como";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(330, 422);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dataGridView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Amorous - Editor de Textos V 1.0.0.1 Alpha";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PathColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OriginalTextColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TranslatedTextColumn;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace TraducAmorous
+{
+    public partial class MainForm : Form
+    {
+        private string currentFilePath;
+        private JObject jsonData;
+        private List<TextItem> textItems = new List<TextItem>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            dataGridView.AutoGenerateColumns = false;
+            dataGridView.CellEndEdit += DataGridView_CellEndEdit;
+            this.Resize += MainForm_Resize;
+
+            // Configurar drag and drop
+            this.AllowDrop = true;
+            this.DragEnter += MainForm_DragEnter;
+            this.DragDrop += MainForm_DragDrop;
+            this.DragLeave += MainForm_DragLeave; // Nuevo evento
+
+            // Inicializar botones deshabilitados
+            btnSave.Enabled = false;
+            btnSaveAs.Enabled = false;
+
+            this.Text = "Amorous - Editor de Textos V" + ProductVersion +" Alpha";
+
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            // Verificar si el objeto arrastrado es un archivo
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Verificar si es un archivo JSON (aunque permitiremos cualquier archivo para flexibilidad)
+                if (files.Length == 1)
+                {
+                    e.Effect = DragDropEffects.Copy;
+                    return;
+                }
+            }
+            e.Effect = DragDropEffects.None;
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 1)
+            {
+                currentFilePath = files[0];
+                LoadJsonFile(currentFilePath);
+            }
+        }
+
+        private void MainForm_DragLeave(object sender, EventArgs e)
+        {
+            // Restaurar el cursor cuando el archivo sale del área del formulario
+            this.Cursor = Cursors.Default;
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (dataGridView.Columns.Count > 0)
+            {
+                int totalWidth = dataGridView.ClientSize.Width;
+                if (totalWidth > 0)
+                {
+                    dataGridView.Columns[0].Width = (int)(totalWidth * 0.2);
+                    dataGridView.Columns[1].Width = (int)(totalWidth * 0.4);
+                    dataGridView.Columns[2].Width = (int)(totalWidth * 0.4);
+                }
+            }
+        }
+
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentFilePath = openFileDialog.FileName;
+                    LoadJsonFile(currentFilePath);
+                }
+            }
+        }
+
+        private void LoadJsonFile(string filePath)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                lblStatus.Text = "Cargando archivo...";
+                Application.DoEvents(); // Para que se actualice el label inmediatamente
+
+                // Verificar que el archivo existe y no está vacío
+                if (!File.Exists(filePath))
+                {
+                    lblStatus.Text = "Archivo no encontrado";
+                    MessageBox.Show("El archivo no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                    return;
+                }
+
+                FileInfo fileInfo = new FileInfo(filePath);
+                if (fileInfo.Length == 0)
+                {
+                    lblStatus.Text = "Archivo vacío";
+                    MessageBox.Show("El archivo está vacío.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                    return;
+                }
+
+                string jsonContent = File.ReadAllText(filePath);
+                jsonData = JObject.Parse(jsonContent);
+                textItems.Clear();
+
+                // Buscar recursivamente todos los campos "Text" en el JSON
+                FindTextFields(jsonData);
+
+                // Mostrar en la grilla
+                dataGridView.DataSource = null;
+                dataGridView.DataSource = textItems;
+                lblStatus.Text = $"Archivo cargado: {Path.GetFileName(filePath)} - {textItems.Count} textos encontrados";
+               
+                // Habilitar botones relacionados
+                btnSave.Enabled = true;
+                btnSaveAs.Enabled = true;
+
+            }
+            catch (JsonException jex)
+            {
+                lblStatus.Text = "Error en formato JSON";
+                MessageBox.Show($"Error en el formato JSON: {jex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Error al cargar archivo";
+                MessageBox.Show($"Error al cargar el archivo: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void FindTextFields(JToken token)
+        {
+            if (token.Type == JTokenType.Object)
+            {
+                foreach (JProperty property in token.Children<JProperty>())
+                {
+                    if (property.Name == "Text" && property.Value.Type == JTokenType.String)
+                    {
+                        // Obtener la ruta completa para poder actualizar después
+                        string path = property.Path;
+                        string text = property.Value.ToString();
+                        textItems.Add(new TextItem { Path = path, OriginalText = text, TranslatedText = text });
+                    }
+                    else
+                    {
+                        FindTextFields(property.Value);
+                    }
+                }
+            }
+            else if (token.Type == JTokenType.Array)
+            {
+                foreach (JToken child in token.Children())
+                {
+                    FindTextFields(child);
+                }
+            }
+        }
+
+        private void DataGridView_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 2 && e.RowIndex >= 0) // Columna de texto traducido
+            {
+                var editedItem = textItems[e.RowIndex];
+                UpdateJsonValue(editedItem.Path, editedItem.TranslatedText);
+            }
+        }
+
+        private void UpdateJsonValue(string path, string newValue)
+        {
+            try
+            {
+                JToken token = jsonData.SelectToken(path);
+                if (token != null)
+                {
+                    token.Replace(newValue);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al actualizar el valor: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                MessageBox.Show("No hay archivo cargado para guardar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                File.WriteAllText(currentFilePath, jsonData.ToString(Formatting.Indented));
+                MessageBox.Show("Archivo guardado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lblStatus.Text = $"Archivo guardado: {Path.GetFileName(currentFilePath)}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar el archivo: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            if (jsonData == null)
+            {
+                MessageBox.Show("No hay datos para guardar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentFilePath = saveFileDialog.FileName;
+                    File.WriteAllText(currentFilePath, jsonData.ToString(Formatting.Indented));
+                    MessageBox.Show("Archivo guardado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    lblStatus.Text = $"Archivo guardado como: {Path.GetFileName(currentFilePath)}";
+                }
+            }
+        }
+
+
+
+    public class TextItem
+    {
+        public string Path { get; set; }
+        public string OriginalText { get; set; }
+        public string TranslatedText { get; set; }
+    }
+
+    }
+}
